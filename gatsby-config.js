e0ad67df066fd2c5c09e4763094b80245c8f177b
Jakub Gajewski <jakub.gajewski@yahoo.com>
2020-09-20 19:18:25 +0100

    pathPrefix change

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Contentful starter',
   },
-  pathPrefix: '/gatsby-contentful-starter',
+  pathPrefix: '/gatsby-playground',
   plugins: [
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
